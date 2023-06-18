712e483a7bed46e35461ddcff5e4b09aed4570a7
Pavel Chashkov <pavelchashkov@gmail.com>
2023-06-18 18:12:30 +0300

    copy src from hw-4

---
+++ terraform-hw-05/src/main.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">=0.13"
+}
+
+provider "yandex" {
+  token     = var.token
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+  zone      = var.default_zone
+}
+
+module "vpc_dev" {
+  source = "./modules/vpc"
+  vpc_name = "develop"
+  vpc_zone = "ru-central1-a"
+  vpc_v4_cidr_blocks = ["10.0.1.0/24"]
+}
+
+module "test-vm" {
+  source          = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  env_name        = "develop"
+  network_id      = module.vpc_dev.vpc_id
+  subnet_zones    = ["ru-central1-a"]
+  subnet_ids      = [ module.vpc_dev.subnet_id ]
+  instance_name   = "web"
+  instance_count  = 2
+  image_family    = "ubuntu-2004-lts"
+  public_ip       = true
+  
+  metadata = {
+      user-data          = data.template_file.cloudinit.rendered #Для демонстрации №3
+      serial-port-enable = 1
+  }
+
+}
+
+#Пример передачи cloud-config в ВМ для демонстрации №3
+data "template_file" "cloudinit" {
+  template = file("./cloud-init.yml")
+  vars = {
+    ssh_key = file(var.ssh_pub_key_file)
+  }
+}
+
