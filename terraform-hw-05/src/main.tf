de0e2ca7cc78bd2e5e600322ddac01cd3233d56a
Pavel Chashkov <pavelchashkov@gmail.com>
2023-06-19 00:41:22 +0300

    hw-05 task-3 (terraform-hotfix)

--- terraform-hw-05/src/main.tf
+++ terraform-hw-05/src/main.tf
@@ -1,7 +1,8 @@
 terraform {
   required_providers {
     yandex = {
-      source = "yandex-cloud/yandex"
+      source  = "yandex-cloud/yandex"
+      version = "0.92.0"
     }
   }
   required_version = ">=0.13"
@@ -34,8 +35,16 @@ module "vpc_dev" {
   vpc_v4_cidr_blocks = ["10.0.1.0/24"]
 }
 
+# tflint-ignore: terraform_required_providers
+data "template_file" "cloudinit" {
+  template = file("./cloud-init.yml")
+  vars     = {
+    ssh_key = file(var.ssh_pub_key_file)
+  }
+}
+
 module "test-vm" {
-  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=1.0.0"
   env_name       = "develop"
   network_id     = module.vpc_dev.vpc_id
   subnet_zones   = ["ru-central1-a"]
@@ -48,15 +57,6 @@ module "test-vm" {
   metadata = {
     user-data          = data.template_file.cloudinit.rendered #Для демонстрации №3
     serial-port-enable = 1
-  }
-
-}
-
-#Пример передачи cloud-config в ВМ для демонстрации №3
-data "template_file" "cloudinit" {
-  template = file("./cloud-init.yml")
-  vars     = {
-    ssh_key = file(var.ssh_pub_key_file)
   }
 }
 
